70924d206ea7dce47c10498b6aec8fbd23f107c3
Keunhong Lee <dlrmsghd@gmail.com>
2016-01-22 15:00:30 +0900

    Basic shiftedint tests works.

--- include/nba/core/shiftedint.hh
+++ include/nba/core/shiftedint.hh
@@ -3,21 +3,186 @@
 
 #include <cstdint>
 #include <type_traits>
+#include <limits>
+#include <cassert>
+#include <exception>
 
 namespace nba {
+
+#ifdef DEBUG
+class PrecisionLossException : public std::exception
+{
+private:
+	const char* msg;
+public:
+	PrecisionLossException(const char* msg) {
+		this->msg = msg;
+	}
+	virtual const char* what() const throw() {
+		return this->msg;
+	}
+};
+#endif
 
 template<typename Int, unsigned Shift>
 class ShiftedInt
 {
-    static_assert(std::is_integral<Int>::value, "Integer type required.");
+private:
+	typedef __uint128_t LARGE_INT;
+	static_assert(std::is_integral<Int>::value, "Integer type required.");
 
-    Int i;
+	Int shifted_value;
 
-public:
-    ShiftedInt(Int initial) : i(initial) { }
+	public:
+	template<typename InputInt>
+	ShiftedInt(const InputInt& initial) {
+		static_assert(std::is_integral<InputInt>::value, "Integer type required.");
+#ifdef DEBUG
+		if(initial > ((LARGE_INT)std::numeric_limits<Int>::max() << Shift))
+			throw PrecisionLossException("input type is too large");
+		if((initial & (((LARGE_INT)1 << Shift) -1)) != 0)
+			throw PrecisionLossException("lower digits are lost");
+#endif
+		shifted_value = (initial >> Shift);
+	}
 
-    ShiftedInt& operator= (Int v) { i = (v >> Shift); return *this; }
-    operator uint32_t() { return (uint32_t) (i << Shift); }
+	//copy constructor
+	ShiftedInt(const ShiftedInt& orig) {
+		shifted_value = orig.shifted_value;
+	}
+
+	template<typename InputInt>
+	ShiftedInt& operator= (const InputInt& v) {
+		static_assert(std::is_integral<InputInt>::value, "Integer type required.");
+#ifdef DEBUG
+		if(v > ((LARGE_INT)std::numeric_limits<Int>::max() << Shift))
+			throw PrecisionLossException("input type is too large");
+		if((v & (((LARGE_INT)1 << Shift) -1)) != 0)
+			throw PrecisionLossException("lower digits are lost");
+#endif
+		shifted_value = (Int)(v >> Shift);
+		return *this;
+	}
+
+	template<typename InputInt>
+	ShiftedInt& operator+= (const InputInt& v) {
+		static_assert(std::is_integral<InputInt>::value, "Integer type required.");
+#ifdef DEBUG
+		if(v > ((LARGE_INT)std::numeric_limits<Int>::max() << Shift))
+			throw PrecisionLossException("input type is too large");
+		if((v & (((LARGE_INT)1 << Shift) -1)) != 0)
+			throw PrecisionLossException("lower digits are lost");
+#endif
+		shifted_value += (Int)(v >> Shift);
+		return *this;
+	}
+
+	template<typename InputInt>
+	const ShiftedInt operator+ (const InputInt& v) {
+		static_assert(std::is_integral<InputInt>::value, "Integer type required.");
+#ifdef DEBUG
+		if(v > ((LARGE_INT)std::numeric_limits<Int>::max() << Shift))
+			throw PrecisionLossException("input type is too large");
+		if((v & (((LARGE_INT)1 << Shift) -1)) != 0)
+			throw PrecisionLossException("lower digits are lost");
+#endif
+		ShiftedInt ret(*this);
+		ret.shifted_value += (Int)(v >> Shift);
+		return ret;
+	}
+
+	template<typename InputInt>
+	ShiftedInt& operator*= (const InputInt& v) {
+		static_assert(std::is_integral<InputInt>::value, "Integer type required.");
+#ifdef DEBUG
+		if(v > ((LARGE_INT)std::numeric_limits<Int>::max() << Shift))
+			throw PrecisionLossException("input type is too large");
+#endif
+		shifted_value *= (Int)(v);
+		return *this;
+	}
+
+	template<typename InputInt>
+	const ShiftedInt operator* (const InputInt& v) {
+		static_assert(std::is_integral<InputInt>::value, "Integer type required.");
+#ifdef DEBUG
+		if(v > ((LARGE_INT)std::numeric_limits<Int>::max() << Shift))
+			throw PrecisionLossException("input type is too large");
+#endif
+		ShiftedInt ret(*this);
+		ret.shifted_value *= (Int)(v);
+		return ret;
+	}
+
+	template<typename InputInt>
+	bool operator== (const InputInt& v) const {
+		static_assert(std::is_integral<InputInt>::value, "Integer type required.");
+#ifdef DEBUG
+		if(v > ((LARGE_INT)std::numeric_limits<Int>::max() << Shift))
+			throw PrecisionLossException("input type is too large");
+		if((v & (((LARGE_INT)1 << Shift) -1)) != 0)
+			throw PrecisionLossException("lower digits are lost");
+#endif
+		return shifted_value == (Int)(v >> Shift);
+	}
+
+	template<typename InputInt>
+	bool operator!= (const InputInt& v) const {
+		static_assert(std::is_integral<InputInt>::value, "Integer type required.");
+#ifdef DEBUG
+		if(v > ((LARGE_INT)std::numeric_limits<Int>::max() << Shift))
+			throw PrecisionLossException("input type is too large");
+		if((v & (((LARGE_INT)1 << Shift) -1)) != 0)
+			throw PrecisionLossException("lower digits are lost");
+#endif
+		return shifted_value != (Int)(v >> Shift);
+	}
+
+
+	ShiftedInt& operator= (const ShiftedInt& v) {
+		shifted_value = v.shifted_value;
+		return *this;
+	}
+
+	ShiftedInt& operator+= (const ShiftedInt& v) {
+		shifted_value += v.shifted_value;
+		return *this;
+	}
+
+	const ShiftedInt operator+ (const ShiftedInt& v) {
+		ShiftedInt ret(*this);
+		ret.shifted_value += v.shifted_value;
+		return ret;
+	}
+
+	ShiftedInt& operator*= (const ShiftedInt& v) {
+		shifted_value *= v.shifted_value;
+		shifted_value <<= Shift;
+		return *this;
+	}
+
+	const ShiftedInt operator* (const ShiftedInt& v) {
+		ShiftedInt ret(*this);
+		ret.shifted_value *= v.shifted_value;
+		ret.shifted_value <<= Shift;
+		return ret;
+	}
+
+	bool operator== (const ShiftedInt& v) const {
+		return shifted_value == v.shifted_value;
+	}
+
+	bool operator!= (const ShiftedInt& v) const {
+		return shifted_value != v.shifted_value;
+	}
+
+	template<typename ReturnInt>
+	ReturnInt as_value() {
+		static_assert(std::numeric_limits<ReturnInt>::max()
+				>= ((LARGE_INT)std::numeric_limits<Int>::max() << Shift),
+				"return type is not large enough.");
+		return (ReturnInt) (shifted_value << Shift);
+	}
 };
 
 } /* endns(nba) */
--- tests/test_core_shiftedint.cc
+++ tests/test_core_shiftedint.cc
@@ -1,3 +1,7 @@
+#ifndef DEBUG
+#define DEBUG
+#endif
+
 #include <nba/core/assert.hh>
 #include <nba/core/shiftedint.hh>
 #include <cstdio>
@@ -5,15 +9,40 @@
 
 using namespace nba;
 
-TEST(CoreShiftedIntTest, StorageSize) {
-    EXPECT_EQ(sizeof(ShiftedInt<uint16_t, 0>), sizeof(uint16_t));
+typedef ShiftedInt<uint16_t, 4> TestInt;
+
+TEST(CoreShiftedIntTest, Precision_Create) {
+	EXPECT_THROW(TestInt(1), PrecisionLossException);
+
+	EXPECT_NO_THROW(TestInt(16));
+}
+
+TEST(CoreShiftedIntTest, Precision_Operation) {
+	auto x = TestInt(16);
+
+	EXPECT_THROW(x = 3, PrecisionLossException);
+	EXPECT_THROW(x == 3, PrecisionLossException);
+	EXPECT_THROW(x != 3, PrecisionLossException);
+	EXPECT_THROW(x += 3, PrecisionLossException);
+	EXPECT_THROW(x + 3, PrecisionLossException);
+	EXPECT_NO_THROW(x *= 3);
+	EXPECT_NO_THROW(x * 3);
+
+	EXPECT_NO_THROW(x = 32);
+	EXPECT_NO_THROW(x == 32);
+	EXPECT_NO_THROW(x != 32);
+	EXPECT_NO_THROW(x += 32);
+	EXPECT_NO_THROW(x + 32);
+	EXPECT_NO_THROW(x *= 32);
+	EXPECT_NO_THROW(x * 32);
 }
 
 TEST(CoreShiftedIntTest, Assignment) {
-    auto x = ShiftedInt<uint16_t, 2>(123);
-    EXPECT_EQ((uint32_t) x, 120) << "Precision loss due to shift should occur.";
-    auto y = ShiftedInt<uint16_t, 2>(120);
-    EXPECT_EQ((uint32_t) y, 120) << "Should be exactly same.";
+    auto x = ShiftedInt<uint16_t, 0>(123);
+    x.as_value<uint32_t>();
+    //EXPECT_EQ((uint32_t) x, 120) << "Precision loss due to shift should occur.";
+    //auto y = ShiftedInt<uint16_t, 2>(120);
+    //EXPECT_EQ((uint32_t) y, 120) << "Should be exactly same.";
 }
 
 // vim: ts=8 sts=4 sw=4 et
