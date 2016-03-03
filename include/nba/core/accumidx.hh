1d78da1bec30d4e821a9c36e91c1469759b8742f
Joongi Kim <joongi@an.kaist.ac.kr>
2016-03-04 00:43:49 +0900

    Add accumulated index calculator with tests.
     * This will replace batch_ids/item_ids arrays for offloading,
       greatly saving the memory footprint.
    
     * Adds a standardized return (error) codes for NBA internal APIs.

---
+++ include/nba/core/accumidx.hh
@@ -0,0 +1,39 @@
+#ifndef __NBA_CORE_ACCUMIDX_HH__
+#define __NBA_CORE_ACCUMIDX_HH__
+
+#include <nba/core/errors.hh>
+#include <type_traits>
+
+namespace nba {
+
+template<typename T>
+static inline nba::error_t get_accum_idx(const T *group_counts,
+                                         const T num_groups,
+                                         const T global_idx,
+                                         T &group_idx,
+                                         T &item_idx)
+{
+    static_assert(std::is_integral<T>::value, "Integer type required.");
+    T sum = 0;
+    T i;
+    bool found = false;
+    for (i = 0; i < num_groups; i++) {
+        if (global_idx >= sum && global_idx < sum + group_counts[i]) {
+            item_idx = global_idx - sum;
+            found = true;
+            break;
+        }
+        sum += group_counts[i];
+    }
+    if (found)
+        group_idx = i;
+    else
+        return NBA_NOT_FOUND;
+    return NBA_SUCCESS;
+}
+
+} // endns(nba)
+
+#endif
+
+// vim: ts=8 sts=4 sw=4 et
